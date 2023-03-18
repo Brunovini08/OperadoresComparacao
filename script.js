4a2602d43e1c223c02d27f1e6500294170f50eb4
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-03-18 13:41:06 -0300

    Operador Comparacao

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+    Operadores de comparacao
+
+    > maior que
+    >= maior que ou igual a
+    < menor que
+    <= menor que ou igual a 
+    == igualdade
+    === igualdade estrita (valor e tipo)
+    != diferente (valor)
+    !== diferente estrito (valor e tipo
+*/
+
+console.log(10 > 5);
+
+console.log(10 >= 5);
+
+console.log(5 < 10)
+
+console.log(5 <= 10);
+
+console.log(10 == 10);
+
+console.log(10 === 10);
+
+console.log(10 != 10)
+
+console.log(10 !== 10);
